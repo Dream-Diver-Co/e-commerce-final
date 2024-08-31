9a1379dbd2e6ec7386f3540b2464e55770da3a53
Sazal-Abdullah <sazalahamed61@gmail.com>
2024-08-31 11:31:36 +0600

    31

--- resources/views/frontend/layouts/master.blade.php
+++ resources/views/frontend/layouts/master.blade.php
@@ -4,7 +4,7 @@
 
 <head>
     <meta charset="utf-8">
-    <title>Dream Ecommerce</title>
+    <title>Dream Shop</title>
     <meta content="width=device-width, initial-scale=1.0" name="viewport">
     <meta content="Dream Shop" name="keywords">
     <meta content="Dream Shop" name="description">
