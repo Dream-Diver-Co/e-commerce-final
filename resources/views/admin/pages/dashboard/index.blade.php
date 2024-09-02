5741057c0a2f17ee95d3c32e1bcf2b056135e095
Sazal-Abdullah <sazalahamed61@gmail.com>
2024-09-02 14:02:45 +0600

    02

--- resources/views/admin/pages/dashboard/index.blade.php
+++ resources/views/admin/pages/dashboard/index.blade.php
@@ -59,6 +59,24 @@
         </div><!-- end card -->
     </div><!-- end col -->
     <div class="col-xl-3 col-md-6">
+        <div class="card">
+            <div class="card-body">
+                <div class="d-flex">
+                    <div class="flex-grow-1">
+                        <p class="text-truncate font-size-14 mb-2">Total Products</p>
+                        <h4 class="mb-2">{{ $totalProducts }}</h4>
+                        <p class="text-muted mb-0"><span class="text-success fw-bold font-size-12 me-2"><i class="ri-arrow-right-up-line me-1 align-middle"></i>{{ $totalProducts }}</span>from previous period</p>
+                    </div>
+                    <div class="avatar-sm">
+                        <span class="avatar-title bg-light text-success rounded-3">
+                            <i class="mdi mdi-currency-btc font-size-24"></i>
+                        </span>
+                    </div>
+                </div>
+            </div><!-- end cardbody -->
+        </div><!-- end card -->
+    </div><!-- end col -->
+    <div class="col-xl-3 col-md-6">
         <div class="card bg-primary">
             <div class="card-body">
                 <div class="d-flex">
@@ -70,24 +88,6 @@
                     <div class="avatar-sm">
                         <span class="avatar-title bg-light text-primary rounded-3">
                             <i class="ri-user-3-line font-size-24"></i>
-                        </span>
-                    </div>
-                </div>
-            </div><!-- end cardbody -->
-        </div><!-- end card -->
-    </div><!-- end col -->
-    <div class="col-xl-3 col-md-6">
-        <div class="card">
-            <div class="card-body">
-                <div class="d-flex">
-                    <div class="flex-grow-1">
-                        <p class="text-truncate font-size-14 mb-2">Unique Visitors</p>
-                        <h4 class="mb-2">{{ $totalProducts }}</h4>
-                        <p class="text-muted mb-0"><span class="text-success fw-bold font-size-12 me-2"><i class="ri-arrow-right-up-line me-1 align-middle"></i>{{ $totalProducts }}</span>from previous period</p>
-                    </div>
-                    <div class="avatar-sm">
-                        <span class="avatar-title bg-light text-success rounded-3">
-                            <i class="mdi mdi-currency-btc font-size-24"></i>
                         </span>
                     </div>
                 </div>
